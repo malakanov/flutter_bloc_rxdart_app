1a2faa778d4824ceb269c4f812218e35850f1fae
malakanov <sergey.malakanov@gmail.com>
2021-02-18 22:45:56 +0500

    add bloc pattern and RxDart

---
+++ android/app/src/main/kotlin/com/calculate/login_block_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.calculate.login_block_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          emailField(bloc),
+          passwordField(bloc),
+          Container(margin: EdgeInsets.only(top: 25)),
+          submitButton(bloc),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: 'examle@mail.ru',
+            labelText: 'Email address',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.password,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changePassword,
+            decoration: InputDecoration(
+              hintText: 'Enter your password',
+              labelText: 'Password field',
+              errorText: snapshot.error,
+            ),
+          );
+        });
+  }
+
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Submit'),
+          color: Colors.blue,
+          onPressed: snapshot.hasData ? bloc.submit : null,
+        );
+      },
+    );
+  }
+}
